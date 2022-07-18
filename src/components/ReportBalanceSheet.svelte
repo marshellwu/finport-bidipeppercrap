ce0259393855881181db030c2bd2e64bccf84548
Marshell Wu <marshellwu@proton.me>
2022-07-18 12:29:16 +0700

    Finished balance sheet recursive logic

---
+++ src/components/ReportBalanceSheet.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import { accounts as mainAccounts } from '../stores';
+    export let accounts = [];
+    export let parentAccount = '';
+    export let parentName = '';
+    export let miscBalance = 0;
+
+    $: childAccounts = accounts.filter(a => a.parentAccount == parentAccount);
+    
+    $: hasChild = parent => accounts.find(a => a.parentAccount == parent.id);
+
+    $: childSum = (parent) => {
+        let childs = accounts.filter(a => a.parentAccount == parent.id);
+        let sum = parent.balance;
+
+        if (childs) {
+            childs.forEach(account => {
+                sum += childSum(account);
+            });
+        }
+
+        return sum;
+    };
+</script>
+
+{#if childAccounts}
+    <ul class="list-group list-group-flush">
+        {#each childAccounts as account}
+            <li class="list-group-item">{account.name} <span class="float-end">{childSum(account)}</span> </li>
+            {#if hasChild(account)}
+                <li class="list-group-item">
+                    <svelte:self accounts={$mainAccounts} parentName={account.name} parentAccount={account.id} miscBalance={account.balance}/>
+                </li>
+            {/if}
+        {/each}
+        {#if miscBalance}
+            <li class="list-group-item">
+                {parentName} {miscBalance}
+            </li>
+        {/if}
+    </ul>
+{/if}
--- src/components/ReportPrint.svelte
+++ src/components/ReportPrint.svelte
@@ -1,7 +1,23 @@
+<script>
+    import { accounts } from '../stores';
+    import ReportBalanceSheet from "./ReportBalanceSheet.svelte";
+
+    $: debit = $accounts.filter(a => a.balanceType == 'DEBIT');
+    $: credit = $accounts.filter(a => a.balanceType == 'CREDIT');
+</script>
+
 <div class="printable container">
     <div class="row">
         <div class="col">
-            <h1 class="mt-5">⚖ Neraca Dagang</h1>
+            <h1 class="mt-5 mb-3">⚖ Neraca Dagang</h1>
+            <div class="row">
+                <div class="col">
+                    <ReportBalanceSheet accounts={debit} />
+                </div>
+                <div class="col">
+                    <ReportBalanceSheet accounts={credit} />
+                </div>
+            </div>
         </div>
     </div>
 </div>
